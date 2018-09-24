b73f275eb04b6daf62aa2a4d4c2ef618b81e58d1
daz1761 <dazgti@btinternet.com>
2018-09-24 20:17:42 +0100

    Initial commit

---
+++ BusinessLayer/EmployeeBusinessLayer.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace BusinessLayer
+{
+    // this will take care of all the business rules and logic
+    public class EmployeeBusinessLayer
+    {
+        // for demo purposes we will have no logic but data access code (ADO.NET)
+
+        public IEnumerable<Employee> Employees // this property which will return a list of employess back
+        {
+            // just a get means read only!
+            get
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+
+                List<Employee> employees = new List<Employee>();
+
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("spGetAllEmployees", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    con.Open();
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        Employee employee = new Employee();
+                        employee.ID = Convert.ToInt32(rdr["Id"]);
+                        employee.Name = rdr["Name"].ToString();
+                        employee.Gender = rdr["Gender"].ToString();
+                        employee.City = rdr["City"].ToString();
+                        employee.DateOfBirth = Convert.ToDateTime(rdr["DateOfBirth"]);
+
+                        employees.Add(employee);
+                    }
+                }
+
+                return employees;
+            }
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            string connectionString =
+            ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spAddEmployee", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter paramID = new SqlParameter();
+                paramID.ParameterName = "@Id";
+                paramID.Value = employee.ID;
+                cmd.Parameters.Add(paramID);
+
+                SqlParameter paramName = new SqlParameter();
+                paramName.ParameterName = "@Name";
+                paramName.Value = employee.Name;
+                cmd.Parameters.Add(paramName);
+
+                SqlParameter paramGender = new SqlParameter();
+                paramGender.ParameterName = "@Gender";
+                paramGender.Value = employee.Gender;
+                cmd.Parameters.Add(paramGender);
+
+                SqlParameter paramCity = new SqlParameter();
+                paramCity.ParameterName = "@City";
+                paramCity.Value = employee.City;
+                cmd.Parameters.Add(paramCity);
+
+                SqlParameter paramDateOfBirth = new SqlParameter();
+                paramDateOfBirth.ParameterName = "@DateOfBirth";
+                paramDateOfBirth.Value = employee.DateOfBirth;
+                cmd.Parameters.Add(paramDateOfBirth);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ MVC2/Controllers/EmployeeController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLayer;
+
+namespace MVC2.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public ActionResult Index()
+        {
+            EmployeeBusinessLayer employeeBusinessLayer = new EmployeeBusinessLayer(); // return IEnumerable
+            List<Employee> employees = employeeBusinessLayer.Employees.ToList();
+
+            return View(employees);
+        }
+
+        [HttpGet]
+        [ActionName("Create")]
+        public ActionResult Create_Get()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public ActionResult Create(FormCollection formCollection)
+        //{
+        //    Employee employee = new Employee();
+
+        //    // Retrieve form data using form collection
+        //    employee.ID = 6;
+        //    employee.Name = formCollection["Name"];
+        //    employee.Gender = formCollection["Gender"];
+        //    employee.City = formCollection["City"];
+        //    employee.DateOfBirth = Convert.ToDateTime(formCollection["DateOfBirth"]);
+
+        //    EmployeeBusinessLayer employeeBusinessLayer = new EmployeeBusinessLayer();
+        //    employeeBusinessLayer.AddEmployee(employee);
+
+        //    //foreach(string key in formCollection.AllKeys)
+        //    //{
+        //    //    Response.Write("Key =" + key + "    ");
+        //    //    Response.Write(formCollection[key] + "   ");
+        //    //    Response.Write("<br/>");
+        //    //}
+
+        //    return RedirectToAction("Index");
+        //}
+
+        //[HttpPost]
+        //// these args are auto mapped from the <input> tags using model binding - note the arg names must match the names of the tags <input name=Name> etc.
+        //public ActionResult Create(int id, string name, string gender, string city, DateTime dateOfBirth)
+        //{
+
+        //    Employee employee = new Employee();
+
+        //    // Retrieve form data using parameters
+        //    employee.ID = id;
+        //    employee.Name = name;
+        //    employee.Gender = gender;
+        //    employee.City = city;
+        //    employee.DateOfBirth = dateOfBirth;
+
+        //    EmployeeBusinessLayer employeeBusinessLayer = new EmployeeBusinessLayer();
+        //    employeeBusinessLayer.AddEmployee(employee);
+
+        //    return RedirectToAction("Index");
+        //}
+
+        [HttpPost]
+        [ActionName("Create")]
+        public ActionResult Create_Post()
+        {
+            if (ModelState.IsValid)
+            {
+                Employee employee = new Employee();
+                UpdateModel(employee); // this will do the model bindings for us
+
+                EmployeeBusinessLayer employeeBusinessLayer = new EmployeeBusinessLayer();
+                employeeBusinessLayer.AddEmployee(employee);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
