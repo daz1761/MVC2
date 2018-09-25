913bb7d119dae451c23d7b83d8bed2d714b6d5f8
daz1761 <dazgti@btinternet.com>
2018-09-25 20:21:04 +0100

    completed part 18

--- BusinessLayer/EmployeeBusinessLayer.cs
+++ BusinessLayer/EmployeeBusinessLayer.cs
@@ -85,5 +85,45 @@ namespace BusinessLayer
                 cmd.ExecuteNonQuery();
             }
         }
+
+        public void SaveEmployee(Employee employee)
+        {
+            string connectionString =
+            ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spSaveEmployee", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter paramID = new SqlParameter();
+                paramID.ParameterName = "@Id";
+                paramID.Value = employee.ID;
+                cmd.Parameters.Add(paramID);
+
+                SqlParameter paramName = new SqlParameter();
+                paramName.ParameterName = "@Name";
+                paramName.Value = employee.Name;
+                cmd.Parameters.Add(paramName);
+
+                SqlParameter paramGender = new SqlParameter();
+                paramGender.ParameterName = "@Gender";
+                paramGender.Value = employee.Gender;
+                cmd.Parameters.Add(paramGender);
+
+                SqlParameter paramCity = new SqlParameter();
+                paramCity.ParameterName = "@City";
+                paramCity.Value = employee.City;
+                cmd.Parameters.Add(paramCity);
+
+                SqlParameter paramDateOfBirth = new SqlParameter();
+                paramDateOfBirth.ParameterName = "@DateOfBirth";
+                paramDateOfBirth.Value = employee.DateOfBirth;
+                cmd.Parameters.Add(paramDateOfBirth);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
     }
 }
--- MVC2/Controllers/EmployeeController.cs
+++ MVC2/Controllers/EmployeeController.cs
@@ -80,9 +80,54 @@ namespace MVC2.Controllers
 
                 EmployeeBusinessLayer employeeBusinessLayer = new EmployeeBusinessLayer();
                 employeeBusinessLayer.AddEmployee(employee);
+
+                return RedirectToAction("Index");
             }
 
-            return RedirectToAction("Index");
+            return View();
         }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            EmployeeBusinessLayer employeeBusinessLayer = new EmployeeBusinessLayer();
+            Employee employee = employeeBusinessLayer.Employees.Single(emp => emp.ID == id);
+
+            return View(employee);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Employee employee)
+        {
+
+            if(ModelState.IsValid)
+            {
+                EmployeeBusinessLayer employeeBusinessLayer = new EmployeeBusinessLayer();
+                employeeBusinessLayer.SaveEmployee(employee);
+
+                return RedirectToAction("Index");
+            }
+            
+            return View(employee);
+        }
+
+        //[HttpPost]
+        //[ActionName("Create")]
+        //public ActionResult Create_Post()
+        //{
+        //    Employee employee = new Employee();
+        //    UpdateModel(employee);
+
+        //    if(ModelState.IsValid)
+        //    {
+        //        EmployeeBusinessLayer employeeBusinessLayer = new EmployeeBusinessLayer();
+        //        employeeBusinessLayer.AddEmployee(employee);
+
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    return View();
+
+        //}
     }
 }
